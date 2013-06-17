27d29aca93f0a1bd1d20ed8610a4e964eecdbdff
Benjamin Wolff <benjamin.wolff@cern.ch>
2013-06-17 19:38:06 +0200

    Reverted changes of EntryPoint implementation due to a signed code SecurityException when deployed to Tomcat.

--- grails-app/services/grails/rwt/example/HelloEntryPointService.groovy
+++ grails-app/services/grails/rwt/example/HelloEntryPointService.groovy
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * Copyright 2012-2013 Benjamin Wolff
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * Contributors:
  *     Benjamin Wolff - initial API and implementation
  ******************************************************************************/
@@ -23,7 +23,7 @@ import org.eclipse.jface.layout.GridLayoutFactory
 import org.eclipse.jface.viewers.ArrayContentProvider
 import org.eclipse.jface.viewers.LabelProvider
 import org.eclipse.jface.viewers.ListViewer
-import org.eclipse.rap.rwt.application.AbstractEntryPoint
+import org.eclipse.rap.rwt.application.EntryPoint
 import org.eclipse.swt.SWT
 import org.eclipse.swt.events.SelectionAdapter
 import org.eclipse.swt.events.SelectionEvent
@@ -35,25 +35,31 @@ import org.eclipse.swt.widgets.Shell
 
 // "Note the use of the Groovy syntax"
 
-class HelloEntryPointService extends AbstractEntryPoint { // "The service needs to implement EntryPoint"
+class HelloEntryPointService implements EntryPoint { // "The service needs to implement EntryPoint"
 
     def fooService // "Other services will get injected by Grails"
 
     @Override
-    protected void createContents(Composite parent) {
+    public int createUI() {
         log.info("Creating RWT UI ...") // "Implicit logger object"
 
-        final Label label = new Label(parent, SWT.NONE)
+        Display display = new Display()
+        Shell shell = new Shell(display)
+//        shell.setSize(500, 400)
+        shell.setText("Hello RWT!")
+        GridLayoutFactory.swtDefaults().applyTo(shell)
+
+        final Label label = new Label(shell, SWT.NONE)
         GridDataFactory.swtDefaults().align(SWT.FILL, SWT.CENTER).grab(true, false).applyTo(label)
 
         // "JFace is available"
-        ListViewer viewer = new ListViewer(parent, SWT.V_SCROLL | SWT.H_SCROLL)
+        ListViewer viewer = new ListViewer(shell, SWT.V_SCROLL | SWT.H_SCROLL)
         GridDataFactory.swtDefaults().hint(500, 300).applyTo(viewer.list)
         viewer.contentProvider = ArrayContentProvider.instance
         viewer.labelProvider = new LabelProvider()
         viewer.input = Foo.list() // "Use domain objects and GORM, no DTOs required!"
 
-        Button button = new Button(parent, SWT.PUSH)
+        Button button = new Button(shell, SWT.PUSH)
         GridDataFactory.swtDefaults().align(SWT.BEGINNING, SWT.CENTER).applyTo(button)
         button.text = "Click me!"
         button.addSelectionListener(new SelectionAdapter() {
@@ -64,13 +70,17 @@ class HelloEntryPointService extends AbstractEntryPoint { // "The service needs 
                 label.text = fooService.serviceMethod(num++) // "Call methods of other services"
             }
         })
-    }
 
-    @Override
-    protected Shell createShell(Display display) {
-        Shell shell = new Shell(display)
-        shell.setText("Hello RWT!")
-        GridLayoutFactory.swtDefaults().applyTo(shell)
-        return shell
+        shell.pack()
+        shell.open()
+
+        while(!shell.isDisposed()) {
+            if(!display.readAndDispatch()) {
+                display.sleep()
+            }
+        }
+        display.dispose()
+
+        return 0;
     }
 }
